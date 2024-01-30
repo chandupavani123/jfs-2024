bf2113ad58ce55a55f0d32da58388c3ffd1474d5
Chandu Pavani <chandupavani@Chandus-MBP.lan>
2024-01-30 15:29:50 -0500

    Initial commit

---
+++ src/main/java/com/vz/SumOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package com.vz;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("The Sum Of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
